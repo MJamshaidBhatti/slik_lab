c53beef3eb1c45acb7c0683b64680f085db18059
jdsab1 <bhatti43667@gmail.com>
2024-05-16 23:15:45 +0500

    image and logo set alignment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,25 @@
 import Container from "react-bootstrap/Container";
-// import{ Img} from 'react-bootstrap';
 import { NavDropdown, Navbar, Nav, Image } from "react-bootstrap";
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from "../assets/image/logo.svg";
 
 function CollapsibleExample() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container fluid>
         <Navbar.Brand href="#home">
-          <div class="logo d-flex justify-content-between">
-            <Image src="assets/image/logo.svg" />
+          <div class="logo d-flex  w-25 justify-content-between">
+            <Image className="logo me-3" src={logo} />
             <div class="d-flex align-items-end flex-column d-non">
-              <span class="title1">aiCarousels</span>
-              <span class="logo-text title2 fw-600">.com</span>
+              <span class="title-3 fw-bold">aiCarousels</span>
+              <span class="logo-text title-1 fw-600">.com</span>
             </div>
           </div>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+        <Navbar.Collapse
+          className="flex-row-reverse"
+          id="responsive-navbar-nav"
+        >
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
